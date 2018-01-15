0df01e67ccab5f73726d18ef16276782946482dc
Bryan Woodhams <Woodhamsbd@gmail.com>
2018-01-15 09:30:07 -0800

    Init CleanUp

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <h1>Welcome to my Website</h1>
+    );
+  }
+}
+
+export default Header;
