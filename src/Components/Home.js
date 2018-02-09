fe9b941bc3aae7f23b43f211f7ed2127a368ef2d
Bryan Woodhams <Woodhamsbd@gmail.com>
2018-02-08 21:12:05 -0800

    Project Page layout before grid reset

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,11 +3,12 @@ import '../Styles/App.css';
 
 
 // Components
-class Home extends React.Component {
+class Home extends Component {
   render() {
     return(
       <div className="home-container">
-        <h1>Home container</h1>
+        <h1 className="center">Welcome to my personal site</h1>
+        <p>You will find links to projects that are in progress under the corresponding link and some information about me.</p>
       </div>
     )
   }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,17 +6,56 @@ class Projects extends Component {
   render() {
     return (
       <div className="projects">
-        {/* TODO: Css classes for each header within navigation render */}
-        {/* TODO: Css classes for each item within these renders - including different classes for project, interest, photos, etc */}
-        <h1>Personal Projects</h1>
-        <p>A paragraph for my first project</p>
-        <p>This will follow with a 4 column width layout with a thumbnail for each project</p>
-        <p>After each thumbnail will be a write up with a quick discription of each project and the stack that it is writen with</p>
-        <p>include:
-          <ul>
-            <li></li>
+        <h1 className="center" >Personal Projects</h1>
+        <p>Here are several of the projects I am currently working on:</p>
+
+        <div className="project">
+          <h3>Stock History and Research</h3>
+          <ul className="project-links">
+            <li><a href="#">Link to Github</a></li>
+            <li><a href="#">Link to URL</a></li>
           </ul>
-        </p>
+          
+          <h4>Stack:</h4>
+          <p>Written with a Node server using Express routing and the axios library to handle API calls to AlphaVantage</p>
+          <p>Front end written in React and utilizing the D3 data visualization library to generate graphs</p>
+
+          <h4>Purpose:</h4>
+          <p>Familiarize myself withe both axios and Alpha Vantage API as well as practice utilizing state management with React.</p>
+          
+        </div>
+
+        <br></br>
+
+        <div className="project">
+          <h3>Office Appointment Scheduling and Template App</h3>
+          <ul className="project-links">
+            <li><a href="#">Link to Github</a></li>
+            <li><a href="#">Link to URL</a></li>
+          </ul>
+
+          <h4>Stack:</h4>
+          <p>Rails 5 backend and front-end templating through standard erb pages.</p>
+
+          <h4>Purpose:</h4>
+          <p>Utilize basic User authentication, familiarize myself with rails calendar gems and learn Word templating using Postgres DB for information storage</p>
+        </div>
+
+        <br></br>
+
+        <div className="project">
+          <h3></h3>
+          <ul className="project-links">
+            <li><a href="#">Link to Github</a></li>
+            <li><a href="#">Link to URL</a></li>
+          </ul>
+
+          <h4>Stack:</h4>
+          <p></p>
+
+          <h4>Purpose:</h4>
+          <p></p>
+        </div>
       </div>
     )
   }
